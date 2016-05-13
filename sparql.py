7f8c3b02df3fe43c8b9ee9ef3d1c4c7fa635ad11
Noah Van Es <noahves@vub.ac.be>
2016-05-13 17:36:37 +0200

    added class for SPARQL queries
    - done: method to get all data for matrix
    - todo: add method to get some random recipes (20?)

--- sparql.py
+++ sparql.py
@@ -1,14 +1,48 @@
 # import credentials
+import pandas as pd
 from config import *
-
-# from rdflib.namespace import RDFS
 from rdflib.plugins.stores import sparqlstore
 from rdflib import Namespace
 
+my_config = {
+	'endpoint': STARDOG_ENDPOINT,	
+	'username': STARDOG_USERNAME,
+	'password': STARDOG_PASSWORD
+}
+
+
+
 class FoodDatabase(object):
 
-	def __init__(self):
+	def __init__(self,configuration):
 		super(FoodDatabase, self).__init__()
-		self.user_matrix = 0
+		#setup the SPARQL endpoint
+		self.store = sparqlstore.SPARQLUpdateStore()
+		self.store.open((configuration['endpoint'],configuration['endpoint']))
+		self.store.setCredentials(configuration['username'],configuration['password'])
+		#setup prefixed for common namespaces
+		self.store.bind('ont',Namespace('http://www.food-groups.com/ontology#'))
+		self.store.bind('dc',Namespace('http://purl.org/dc/elements/1.1/'))
 
-	def 
+	def load_data(self):
+		query = """SELECT (count(?recipe) as ?count) ?uploader ?ingredient WHERE 
+				{
+					 ?recipe a ont:Recipe;
+					         dc:creator ?uploader;
+					         ont:requiresQuantityOfIngredient ?ingredientQuantity.
+					 ?ingredientQuantity ont:hasIngredient ?ingredient.
+				} GROUP BY ?uploader ?ingredient""" 
+		uploader_dct = {} #start with empty dct
+		for result in self.store.query(query):
+			uploader = result.uploader.toPython()
+			if uploader not in uploader_dct:
+				uploader_dct[uploader] = {}
+			ingredient_dct = uploader_dct[uploader]
+			ingredient = result.ingredient.toPython()
+			ingredient_dct[ingredient] = result[0].toPython()
+		return pd.DataFrame.from_dict(uploader_dct).fillna(0)
+
+if __name__ == '__main__':
+	sparqldb = FoodDatabase(my_config)
+	results = sparqldb.load_data()
+	print(str(results))
