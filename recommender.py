74613e868ef89617c8c24b76b0cd7beb704fe766
JensNevens <jens.nevens@gmail.com>
2016-05-14 15:31:26 +0200

    Simple recommendation based on weighted average

--- recommender.py
+++ recommender.py
@@ -1,61 +1,102 @@
-import numpy as np
-import pandas as pd
 from scipy.spatial.distance import euclidean
 from scipy.stats.stats import pearsonr
-from math import sqrt
+from sparql import FoodDatabase
+from config import *
+import numpy as np
+
 
 class Recommender(object):
 
-	def __init__(self):
-		super(Recommender, self).__init__()
-		self.user_matrix = 0
+    def __init__(self, foodDB):
+        super(Recommender, self).__init__()
+        self.foodDB = foodDB
 
-	# Loads the data and transforms it in matrix notation
-	# !!!Temporary Assumption: data comes from Noah's SPARQL query (see Slack)
-	def load_data(self, data):
-		user_dict = {}
-		for idx, row in data.iterrows():
-			if row['uploader'] in user_dict:
-				ingredient_dict = user_dict[row['uploader']]
-				if row['ingredientName'] in ingredient_dict:
-					ingredient_dict[row['ingredientName']] += row['count']
-				else:
-					ingredient_dict[row['ingredientName']] = row['count']
-			else:
-				user_dict[row['uploader']] = {row['ingredientName']: row['count']}
-		self.user_matrix = pd.DataFrame.from_dict(user_dict).fillna(0)
-		return
+    # User x Ingredient matrix where each entry
+    # specifies how often the user used the ingredient
+    def get_user_ingredient_matrix(self):
+        return self.foodDB.user_ingredient_matrix()
 
+    # Recipe x Ingredient matrix where each entry
+    # specifies whether the ingredient is in the recipe
+    def get_recipe_ingredient_matrix(self, limit=20):
+        return self.foodDB.recipe_ingredient_matrix(limit)
 
-	# Returns the Euclidian distance between two users
-	def euclidian(self, user1, user2):
-		return euclidean(self.user_matrix.loc[:,user1], self.user_matrix.loc[:,user2])
+    # Returns all recipes of a user
+    def get_user_recipes(self, user):
+        return self.foodDB.user_recipes(user)
 
-	# Returns the Pearson correlation coefficient between two users
-	def pearson(self, user1, user2):
-		return pearsonr(self.user_matrix.loc[:,user1], self.user_matrix.loc[:,user2])[0]
+    # Returns the name of a recipe
+    def get_recipe_name(self, recipe):
+        return self.foodDB.recipe_name(recipe)
 
-	# Returns the best matches for the given user from the matrix
-	# Number of the results and similarity function are optional parameters
-	def best_matches(self, user, n=5, similarity='pearson'):
-		if similarity == 'pearson':
-			scores = [(self.pearson(user,other), other) for other in self.user_matrix.columns.values if other != user]
-		elif similarity == 'euclidean':
-			scores = [(self.euclidean(user,other), other) for other in self.user_matrix.columns.values if other != user]
-		scores.sort()
-		scores.reverse()
-		return scores[:n]
+    # Returns the uploader of a recipe
+    def get_recipe_uploader(self, recipe):
+        return self.foodDB.recipe_uploader(recipe)
 
-	# My suggestions for the recommender:
-	# [1] get some random recipes, assign each of them a score by doing a weighted average over the recipe's ingredients, where
-	#		x_i = i-th ingredient in the recipe
-	#		w_i = frequency of i-th ingredient in user-ingredient matrix
-	# [2] what you are currently doing: do correlations between users (pearson/euclidiean/...),
-	#		then get a random recipe from the highest scoring user (?)
-	# maybe experiment with both approaches, all the methods needed for these two approaches should be available now :)
+    # Get random recipes. Each recipes gets a score S_i = sum(x_i * w_i)
+    # where x_i is the i'th ingredient of the recipe
+    # w_i is the frequency of the ith ingredient in the user-ingredient matrix
+    # The user gets the recipe with the highest score, since this recipe
+    # has the most ingredients already used by the user.
+    # Maybe include a check so that a user does not get recommended
+    # it's own recipe?
+    def best_matches(self, user, avoid_self=True):
+        recipe_ingredient = self.get_recipe_ingredient_matrix(3)
+        user_ingredient = self.get_user_ingredient_matrix()
+        recipes = recipe_ingredient.columns.values
+        num_recipes = len(recipes)
+        scores = np.zeros(num_recipes)
+        for i in range(num_recipes):
+            ingredient_row = recipe_ingredient.iloc[:, i]
+            for ingredient, used in ingredient_row.iteritems():
+                scores[i] += used * user_ingredient.loc[ingredient, user]
+        recommend = recipes[np.argmax(scores)]
+        while avoid_self:
+            uploader = self.get_recipe_uploader(recommend)
+            if uploader == user:
+                recipes = np.delete(recipes, np.argmax(scores))
+                scores = np.delete(scores, np.argmax(scores))
+                recommend = recipes[np.argmax(scores)]
+            else:
+                break
+        recipe = self.get_recipe_name(recommend)
+        print('We would recommend recipe', recipe)
+        return
+
+    # Returns the Euclidian distance between two users
+    def euclidian(self, user1, user2):
+        return euclidean(self.user_matrix.loc[:, user1],
+                         self.user_matrix.loc[:, user2])
+
+    # Returns the Pearson correlation coefficient between two users
+    def pearson(self, user1, user2):
+        return pearsonr(self.user_matrix.loc[:, user1],
+                        self.user_matrix.loc[:, user2])[0]
+
+    # Returns the most similar user for the given user from the matrix
+    # Number of the results and similarity function are optional parameters
+    def similar_users(self, user, n=5, similarity='pearson'):
+        if similarity == 'pearson':
+            scores = [(self.pearson(user, other), other) for other in self.user_matrix.columns.values if other != user]
+        elif similarity == 'euclidean':
+            scores = [(self.euclidean(user, other), other) for other in self.user_matrix.columns.values if other != user]
+        scores.sort()
+        scores.reverse()
+        return scores[:n]
+
+    
+    # [2] what you are currently doing: correlations between users
+    #     (pearson/euclidiean/...), then get a random recipe from
+    #      the highest scoring user (?)
+    # maybe experiment with both approaches,
+    # all the methods needed for these two approaches should be available now
 
 if __name__ == '__main__':
-	data = pd.read_csv('./export.csv')
-	recsys = Recommender()
-	recsys.load_data(data)
-	print(recsys.best_matches('jens.nevens@vub.ac.be'))
+    stardog = {
+        'endpoint': STARDOG_ENDPOINT,
+        'username': STARDOG_USERNAME,
+        'password': STARDOG_PASSWORD
+        }
+    foodDB = FoodDatabase(stardog)
+    recsys = Recommender(foodDB)
+    recsys.best_matches('jens.nevens@vub.ac.be')
--- sparql.py
+++ sparql.py
@@ -4,77 +4,98 @@ from config import *
 from rdflib.plugins.stores import sparqlstore
 from rdflib import Namespace
 
-my_config = {
-	'endpoint': STARDOG_ENDPOINT,	
-	'username': STARDOG_USERNAME,
-	'password': STARDOG_PASSWORD
-}
-
 
 class FoodDatabase(object):
 
-	def __init__(self,configuration):
-		super(FoodDatabase, self).__init__()
-		#setup the SPARQL endpoint
-		self.store = sparqlstore.SPARQLUpdateStore()
-		self.store.open((configuration['endpoint'],configuration['endpoint']))
-		self.store.setCredentials(configuration['username'],configuration['password'])
-		#setup prefixed for common namespaces
-		self.store.bind('ont',Namespace('http://www.food-groups.com/ontology#'))
-		self.store.bind('dc',Namespace('http://purl.org/dc/elements/1.1/'))
+    def __init__(self, configuration):
+        super(FoodDatabase, self).__init__()
+        # Setup the SPARQL endpoint
+        self.store = sparqlstore.SPARQLUpdateStore()
+        self.store.open((configuration['endpoint'], configuration['endpoint']))
+        self.store.setCredentials(configuration['username'], configuration['password'])
+        # Setup prefixed for common namespaces
+        self.store.bind('ont', Namespace('http://www.food-groups.com/ontology#'))
+        self.store.bind('dc', Namespace('http://purl.org/dc/elements/1.1/'))
 
-	def load_data(self):
-		query = """SELECT (count(?recipe) as ?count) ?uploader ?ingredient WHERE 
-				{
-				 ?recipe a ont:Recipe;
-				         dc:creator ?uploader;
-				         ont:requiresQuantityOfIngredient/ont:hasIngredient ?ingredient.
-				} GROUP BY ?uploader ?ingredient""" 
-		uploader_dct = {} #start with empty dct
-		for result in self.store.query(query):
-			uploader = result.uploader.toPython()
-			if uploader not in uploader_dct:
-				uploader_dct[uploader] = {}
-			ingredient_dct = uploader_dct[uploader]
-			ingredient = result.ingredient.toPython()
-			ingredient_dct[ingredient] = result[0].toPython()
-		return pd.DataFrame.from_dict(uploader_dct).fillna(0)
+    def user_ingredient_matrix(self):
+        query = """
+                SELECT (count(?recipe) as ?count) ?uploader ?ingredient WHERE
+                {
+                 ?recipe a ont:Recipe;
+                         dc:creator ?uploader;
+                         ont:requiresQuantityOfIngredient/ont:hasIngredient ?ingredient.
+                } GROUP BY ?uploader ?ingredient
+                """
+        uploader_dct = {}  # start with empty dct
+        for result in self.store.query(query):
+            uploader = result.uploader.toPython()
+            if uploader not in uploader_dct:
+                uploader_dct[uploader] = {}
+            ingredient_dct = uploader_dct[uploader]
+            ingredient = result.ingredient.toPython()
+            ingredient_dct[ingredient] = result[0].toPython()
+        return pd.DataFrame.from_dict(uploader_dct).fillna(0)
 
-	def load_recipes(self,limit=20):
-		query = """SELECT ?recipe ?ingredient WHERE 
-				{
-				 	{ 
-				      SELECT ?recipe WHERE 
-				      { 
-				        ?recipe a ont:Recipe. 
-				      } ORDER BY RAND() LIMIT %d
-				    }
-				    ?recipe ont:requiresQuantityOfIngredient/ont:hasIngredient ?ingredient.
-				}""" % limit
-		recipe_dct = {}
-		for result in self.store.query(query):
-			recipe = result.recipe.toPython()
-			if recipe not in recipe_dct:
-				recipe_dct[recipe] = {}
-			ingredient_dct = recipe_dct[recipe]
-			ingredient = result.ingredient.toPython()
-			ingredient_dct[ingredient] = True #recipe uses this ingredient
-		return pd.DataFrame.from_dict(recipe_dct).fillna(False)
+    def recipe_ingredient_matrix(self, limit=20):
+        query = """
+                SELECT ?recipe ?ingredient WHERE
+                {
+                    {
+                      SELECT ?recipe WHERE
+                      {
+                        ?recipe a ont:Recipe.
+                      } ORDER BY RAND() LIMIT %d
+                    }
+                    ?recipe ont:requiresQuantityOfIngredient/ont:hasIngredient ?ingredient.
+                }""" % limit
+        recipe_dct = {}
+        for result in self.store.query(query):
+            recipe = result.recipe.toPython()
+            if recipe not in recipe_dct:
+                recipe_dct[recipe] = {}
+            ingredient_dct = recipe_dct[recipe]
+            ingredient = result.ingredient.toPython()
+            ingredient_dct[ingredient] = True  # Recipe uses this ingredient
+        return pd.DataFrame.from_dict(recipe_dct).fillna(False)
 
-	def load_user_recipes(self,user):
-		query = """SELECT ?recipe WHERE 
-					{
-					    ?recipe dc:creator \"%s\";
-					    		a ont:Recipe.
-					}""" % user
-		results = self.store.query(query)
-		results = map(lambda r: r.recipe.toPython(),results)
-		return list(results) #explicit list conversion
+    def user_recipes(self, user):
+        query = """
+                SELECT ?recipe WHERE
+                {
+                    ?recipe dc:creator \"%s\";
+                            a ont:Recipe.
+                }
+                """ % user
+        results = self.store.query(query)
+        results = map(lambda r: r.recipe.toPython(), results)
+        return list(results)  # Explicit list conversion
 
-if __name__ == '__main__':
-	#example usage
-	sparqldb = FoodDatabase(my_config)
-	#results = sparqlstore.load_data()								# load all user-recipe data for the recommender matrix
-	#results = sparqldb.load_recipes(3)  							# load a maximum of 3 random recipes and their ingredients
-	results = sparqldb.load_user_recipes("noah.van.es@vub.ac.be") 	# load all recipes of a user
-	print(results)
+    def recipe_name(self, recipe):
+        query = """
+                SELECT ?recipeName WHERE
+                {
+                    <%s> ont:resultsIn/ont:hasFoodName ?recipeName.
+                }
+                """ % recipe
+        results = self.store.query(query)
+        results = map(lambda r: r.recipeName.toPython(), results)
+        return list(results)[0]  # Assumption: Recipe has only 1 name
+
+    def recipe_uploader(self, recipe):
+        query = """
+                SELECT ?uploader WHERE
+                {
+                    <%s> dc:creator ?uploader.
+                }
+                """ % recipe
+        results = self.store.query(query)
+        results = map(lambda r: r.uploader.toPython(), results)
+        return list(results)[0]  # Assumption: Recipe has only 1 uploader
+
+
+# if __name__ == '__main__':
+#     stardog = {
+#         'endpoint': STARDOG_ENDPOINT,
+#         'username': STARDOG_USERNAME,
+#         'password': STARDOG_PASSWORD
+#         }
